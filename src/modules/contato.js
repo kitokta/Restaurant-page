2d82230a71a4950682f42f8dbbf64bd44959bede
Gabi Zanini <psicologia.plus@gmail.com>
2022-08-17 11:31:37 -0300

    Basic contact page working

--- src/modules/contato.js
+++ src/modules/contato.js
@@ -1,13 +1,64 @@
 let counter = 0;
+import mail from '../assets/icons/gmail.png';
+import whatsapp from '../assets/icons/whatsapp.png';
+import instagram from '../assets/icons/instagram.png';
+
 
 export default class contato {
   static load(content) {
     const title = document.createElement("h1");
     title.innerHTML = "Entre em contato conosco!";
     title.setAttribute("id", "title");
+
+    //text content (whatsapp)
+    const textDiv1 = document.createElement('div');
+    const textContent1 = document.createElement('h2');
+    textContent1.innerHTML = "(55) 95555-5555"
+    textDiv1.classList.add('text-div-grid', 'text-div');
+    textDiv1.appendChild(textContent1);
+
+    //textDiv1 Image
+    const whatsappIcon = document.createElement('img');
+    whatsappIcon.src = whatsapp;
+    whatsappIcon.alt = 'whatsapp-icon';
+    whatsappIcon.style.height = '50%';
+    textDiv1.appendChild(whatsappIcon);
+
+    //text content (instagram)
+    const textDiv2 = document.createElement('div');
+    const textContent2 = document.createElement('h3');
+    textContent2.innerHTML = "@padariacoreanaverdadeira"
+    textDiv2.classList.add('text-div-grid', 'text-div');
+    textDiv2.appendChild(textContent2);
+
+    //textDiv2 Image
+    const instagramIcon = document.createElement('img');
+    instagramIcon.src = instagram;
+    instagramIcon.alt = 'instagram-icon';
+    instagramIcon.style.height = '50%';
+    textDiv2.appendChild(instagramIcon);
+
+    //text content (mail)
+    const textDiv3 = document.createElement('div');
+    const textContent3 = document.createElement('h3');
+    textContent3.innerHTML = "emailverdadeiro@naosoufalso.com"
+    textDiv3.classList.add('text-div-grid', 'text-div');
+    textDiv3.appendChild(textContent3);
+
+    //textDiv1 Image
+    const mailIcon = document.createElement('img');
+    mailIcon.src = mail;
+    mailIcon.alt = 'mail-icon';
+    mailIcon.style.height = '50%';
+    textDiv3.appendChild(mailIcon);
+
+
     const box = document.createElement("div");
     box.setAttribute("id", "box");
     box.appendChild(title);
+    box.appendChild(textDiv1);
+    box.appendChild(textDiv2);
+    box.appendChild(textDiv3);
     content.appendChild(box);
     contato.counter("add");
   }
